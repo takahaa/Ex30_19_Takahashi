60dcf0e36937d2bf84840bbc979f6db559acebb0
takahaa <Mametubu0219Mazuru@icloud.com>
2022-10-27 17:27:58 +0900

    tuika

---
+++ Ex28_template/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Cat : Animal
+    {
+        public Cat (DateTime bd, string n, float w = 0, float h = 0, float d = 0) : base(bd, n, w, h, d)
+        {
+
+        }
+    }
+}
